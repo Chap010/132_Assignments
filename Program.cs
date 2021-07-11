3d4b1c17b99d0723359a8399ef48b53e48cdd174
Antonio <atejda8071@southwestuniversity.edu>
2021-07-11 15:00:50 -0600

    Added Week 2 Assignments

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Week2Assignment_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PastryBox pastries = new PastryBox();
+            pastries.items = 10;
+            pastries.chocolateChipCookies = 4;
+            pastries.brownies = 3;
+            pastries.oatmealCookies = 3;
+            pastries.brand = "Pancho's Bakery";
+
+            Console.WriteLine($"Welcome To {pastries.brand}");
+            Console.ReadLine();
+
+
+        }
+
+    }
+    class PastryBox
+    {
+        public int items;
+        public int chocolateChipCookies;
+        public int brownies;
+        public int oatmealCookies;
+        public string brand;
+
+    }
+}
